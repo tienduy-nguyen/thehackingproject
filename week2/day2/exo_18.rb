cec7308b58469f742769232585cec83dc4f0747b
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-07-08 09:28:13 +0200

    Get list email to array

--- week2/day2/exo_18.rb
+++ week2/day2/exo_18.rb
@@ -1,9 +1,14 @@
 puts "List of fake emails"
 
+emails = []
+count = 0
 50.times do |num|
   if num < 9
-    puts "jean.dupont.0#{num+1}@email.fr"
+    emails[count] = "jean.dupont.0#{num+1}@email.fr"
   else
-    puts "jean.dupont.#{num+1}@email.fr"
+    emails[count] = "jean.dupont.#{num+1}@email.fr"
   end
+  count += 1
 end
+
+puts emails
--- week2/day2/exo_19.rb
+++ week2/day2/exo_19.rb
@@ -1,12 +1,18 @@
 puts "List of pair emails"
 
+emails = []
+count = 0
+
 50.times do |num|
   true_num  = num +1
   if (true_num % 2 == 0)
     if true_num < 9
-      puts "jean.dupont.0#{true_num}@email.fr"
+      emails[count] = "jean.dupont.0#{true_num}@email.fr"
     else
-      puts "jean.dupont.#{true_num}@email.fr"
+      emails[count] = "jean.dupont.#{true_num}@email.fr"
     end
+    count +=1
   end  
 end
+
+puts emails

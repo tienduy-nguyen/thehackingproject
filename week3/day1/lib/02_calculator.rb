50620f824e87af1b3314e82e15b97257a91a9db9
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-07-14 00:06:40 +0200

    Update

--- week3/day1/lib/02_calculator.rb
+++ week3/day1/lib/02_calculator.rb
@@ -1,3 +1,19 @@
 def add(a,b)
+  a+b
+end
+def subtract(a,b)
+  a-b
+end
+def sum(array)
+array.reduce(0,:+)
+end
 
+def multiply(a,b)
+  a*b
+end 
+def power(a,n)
+  a**n
+end
+def factorial(n)
+  (1..n).inject(:*) || 1
 end

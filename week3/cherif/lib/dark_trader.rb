7ae065310e7f0aab9c81cca7f8c109b88634bf27
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-07-16 20:26:45 +0200

    Update exe 1 for Cherif

--- week3/cherif/lib/dark_trader.rb
+++ week3/cherif/lib/dark_trader.rb
@@ -1,20 +1,26 @@
-
 require 'nokogiri'
 require 'open-uri'
 
-# Fetch and parse HTML document
-doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
 
-doc.xpath('//table/tbody/tr').each do |row|
-  # In each row, we will get the infomations we need
-    crypto_name_node = row.at_xpath('td[contains(@class,"cmc-table__cell--sort-by__symbol")]')
+# Boucle pour chaque ligne de crypto
+def get_cryto
+  doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
+  results = []
+  doc.xpath('//table/tbody/tr').each do |row|
+    # Accéeder colonnes qu'on n'a besoin 
+    cryto_name_node = row.at_xpath('td[contains(@class,"cmc-table__cell--sort-by__symbol")]')
     price_node = row.at_xpath('td[contains(@class,"cmc-table__cell--sort-by__price")]/a')
-    crypto_name = crypto_name_node.children.text
-    price_value = price_node.children.text[1..-1].gsub(/[^\d^.]/,'').to_f # convert humain number string to fload
+  
+    cryto_name = cryto_name_node.text
+    price = price_node.text[1..-1]
+  
+    hash  =  Hash.new
+    hash[cryto_name] = price
+    results.push(hash)
     
-    # Push hash to an array
-    cryptocurrency = Hash.new
-    cryptocurrency[crypto_name] = price_value
-    results.push(cryptocurrency)
+  end
+  return results
 end
-return results
+
+p get_cryto
+

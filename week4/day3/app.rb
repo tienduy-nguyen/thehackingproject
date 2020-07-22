6633bc311f7a9e9b23ff4c6265e775a561ad148d
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-07-22 10:49:37 +0200

    Add json, google_drive gem library

--- week4/day3/app.rb
+++ week4/day3/app.rb
@@ -1,7 +1,11 @@
 require 'bundler'
 Bundler.require
 
-$:.unshift File.expand_path("lib", __FILE__)
-require'scrapper'
+# $:.unshift File.expand_path("lib", __FILE__)
+$LOAD_PATH << 'lib'
+require'app/scrapper'
+require'app/email_sender'
+require'views/done'
+require'views/index'
 
 Scrapper.new.perform

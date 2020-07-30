b1916b7811a7d89b8cc87bba24aa56a2cc384819
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-07-31 00:20:13 +0200

    Update README.md

--- week5/day4/airbnb_app/db/seeds.rb
+++ week5/day4/airbnb_app/db/seeds.rb
@@ -1,21 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Initilize datas
 Dogsitter.destroy_all
 Dog.destroy_all
 Troll.destroy_all
 City.destroy_all
 
+# Create Cities
 city1 = City.create(name: "Paris01")
 city2 = City.create(name: "Paris02")
 
-
+# Create Dogsitters
 ds1 = Dogsitter.create(name: "Hana", sex: "Female", age: 19, city: city1)
 ds2 = Dogsitter.create(name: "Karen", sex: "Female", age: 22, city: city2)
+
 
 dog1= Dog.create(name: "Dada", sex: "Male", city: city1)
 dog2 = Dog.create(name: "Donie", sex: "Female", city: city2)
--- week5/day4/free_doc_app/db/seeds.rb
+++ week5/day4/free_doc_app/db/seeds.rb
@@ -1,28 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Initialize datas
 Doctor.destroy_all
 Patient.destroy_all
 Appointment.destroy_all
+City.destroy_all
 
+# Create Cities
 city1 = City.create(name: "Paris01", zip_code:"75001")
 city2 = City.create(name: "Paris02", zip_code:"75002")
 city3 = City.create(name: "Paris03", zip_code:"75003")
 city4 = City.create(name: "Paris04", zip_code:"75004")
 
+# Create Doctors
 doc1 = Doctor.create(first_name: "Adame", last_name: "Levil", city: city1)
 doc2 = Doctor.create(first_name: "Alan", last_name: "Walker", city: city2)
 
+# Create Patients
 pat1 = Patient.create(first_name: "Dua", last_name: "Lipa", city: city2)
 pat2 = Patient.create(first_name: "Adele", last_name: "Laurie", city: city3)
 
+# Create Specialities
 doc1.specialties.create(name: "SA")
 doc2.specialties.create(name: "SB")
 
+# Create Appointements
 Appointment.create(doctor: doc1, patient: pat1, city: city4)
 Appointment.create(doctor: doc2, patient: pat2, city: city4)
 

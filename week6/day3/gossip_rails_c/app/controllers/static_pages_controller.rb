0048f6d3edf27b47780ec741a75819a62bb8e696
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-05 21:38:17 +0200

    Use carousel home page

--- week6/day3/gossip_rails_c/app/controllers/static_pages_controller.rb
+++ week6/day3/gossip_rails_c/app/controllers/static_pages_controller.rb
@@ -8,6 +8,12 @@ class StaticPagesController < ApplicationController
   def home
     @first_name = params[:first_name] 
     @gossips = Gossip.all.order(updated_at: :desc)
+    @lastest_gossips = @gossips.take(16)
+    @lastest_1 = @lastest_gossips[0..3]
+    @lastest_2 = @lastest_gossips[4..7]
+    @lastest_3= @lastest_gossips[8..11]
+    @lastest_4 = @lastest_gossips[12..15]
+
     # @tags = @gossip.gossip_tags
     # p @tags
   end
